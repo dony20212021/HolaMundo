71f9f08040b675d085398451313d97d2f7b5a7d0
dony20212021 <81033056+dony20212021@users.noreply.github.com>
2021-03-20 10:31:56 -0600

    Add files via upload

---
+++ App2/App2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HolaMundo");
+
+            Console.ReadKey();
+        }
+    }
+}
